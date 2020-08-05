a7163d3e98e8bdff31453813b2cce7d6291edbdc
ac19-1 <audrey.chen@binus.ac.id>
2020-08-05 23:50:26 +0700

    install bootstrap vue + apply google font

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,11 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat&display=swap'
+      }
     ]
   },
   /*
@@ -53,6 +57,7 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    'bootstrap-vue/nuxt'
   ],
   /*
   ** Axios module configuration
