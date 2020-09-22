80eca6230d643c55c075787c85d077b913b5a90a
ac19-1 <audrey.chen@binus.ac.id>
2020-09-22 17:57:46 +0700

    moussebite

--- store/works.js
+++ store/works.js
@@ -72,6 +72,48 @@ export const state = () => ({
         ],
         description: 'Clovery is a simple website for demonstrating Middleware and Custom Error Page in Laravel. All the data and images in this website are dummy and are stored in MySQL database, and this website is not made for commercial purposes. This website is about an ornamental plant shop, where the guest only can visit the home page, the user can visit home and products page, and admin can visit all pages. You can log in as admin with \"audrey@clovery.com\" as the email address and \"audrey123\" as the password. This website is not responsive and using 1366x768 device is recommended.',
         link: 'http://clovery.dreyychen.com'
+      },
+	  { 
+        id: 'moussebite',
+        name: 'MousseBite', 
+        folder: 'moussebite',
+        image: {
+          name: 'home.png',
+          description: 'Home Page'
+        },
+        type: 'website',
+        all_images : [
+          {
+            name: 'home.png',
+            description: 'Home Page'
+          },
+          {
+            name: 'mousse.png',
+            description: 'Mousse Page'
+          },
+          {
+            name: 'cart.png',
+            description: 'Cart Page'
+          },
+          {
+            name: 'profile.png',
+            description: 'Profile Page'
+          },
+          {
+            name: 'about.png',
+            description: 'About Page'
+          },
+          {
+            name: 'login.png',
+            description: 'Login Page'
+          },
+		  {
+            name: 'register.png',
+            description: 'Register Page'
+          },
+        ],
+        description: 'MousseBite is a simple website for Entrepreneurship project. All of the data in this website are dummy data and are stored in MySQL database, and this website is not made for commercial purposes. This website is about mousse dessert online store, where we can view all the products, add to cart, buy the mousse, and view the transaction history. This website is not responsive and using 1366x768 device is recommended.',
+        link: 'http://moussebite.dreyychen.com'
       }
     ],
     non_website: [
